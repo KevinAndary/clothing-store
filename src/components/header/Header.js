d01e15853773b38c643a3135f2377ef8500e85d4
Kevin <66994542+KevinAndary@users.noreply.github.com>
2023-09-05 23:29:35 +0300

    Add files via upload

---
+++ src/components/header/Header.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleBtnCart, toggleSearchForm } from "../../redux/products/products.slice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import { CartShopping } from "../cart-shopping/CartShopping";
+import { Search } from "../search/Search";
+
+export function Header() {
+  // Get the cart and btnCart state values from the Redux store
+  const { cart, btnCart } = useSelector(state => state.product);
+  
+  // Get the dispatch function from the useDispatch hook
+  const dispatch = useDispatch();
+
+  // Function to toggle the cart visibility
+  const toggleCart = () => dispatch(toggleBtnCart(true));
+  
+  // Function to toggle the search form visibility
+  const toggleSearch = () => dispatch(toggleSearchForm(true));
+
+  // Change the body overflow property based on the btnCart state value
+  if (btnCart) {
+    document.querySelector('body').style.overflow = 'hidden';
+  } else {
+    document.querySelector('body').style.overflow = 'visible';
+  }
+
+  return (
+    // The header element with some classes
+    <header className='w-screen bg-[#221f1f] fixed z-10 top-0 h-[50px]'>
+      {/* A container for the header with some classes */}
+      <div className='container text-lg flex justify-between items-center text-white h-[50px] max-w-[1240px] m-auto px-2'>
+        {/* The logo */}
+        <a href='/' >React Shop</a>
+        
+        {/* The search input */}
+        <Search />
+        
+        {/* A list of buttons */}
+        <ul className='flex justify-between'>
+          {/* The search button */}
+          <button className='block md:hidden mr-2 px-[8px]' onClick={toggleSearch}>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+          
+          {/* The user button */}
+          <li className='mr-2 px-[8px] py-1 rounded hover:bg-gray-600 transition cursor-not-allowed md:mr-4'>
+            <FontAwesomeIcon icon={faUser} className='text-xl'/>
+          </li>
+          
+          {/* The cart button */}
+          <button className='px-[6px] py-1 rounded hover:bg-gray-600 transition relative' onClick={toggleCart}>
+            <FontAwesomeIcon icon={faCartShopping} className='text-xl'/>
+            {/* Show the number of items in the cart */}
+            {cart.length > 0 && <span className='absolute right-[-7px] top-0 bg-[#00a046] text-[12px] h-[10px] flex items-center justify-center px-[7px] py-[10px] rounded-full'>{cart.length}</span>}
+          </button>
+        </ul>
+      </div>
+      {/* The cart overlay */}
+      {btnCart && <CartShopping cart={cart}/>}
+    </header>
+  );
+}
